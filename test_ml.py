8941fc1b0df7ecb1112af6ec7179b5f08b252873
Clinton Forbes <clintonforbes8@example.com>
2024-06-03 07:34:43 -0500

    update 3 tests

--- test_ml.py
+++ test_ml.py
@@ -1,7 +1,9 @@
 import pytest
-import pandas as pd
 import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 from ml.data import process_data
 from ml.model import train_model, compute_model_metrics
 
